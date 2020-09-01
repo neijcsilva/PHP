ee20866374dd59f84b704b5a9e5acfc7fa2e898c
neijcsilva <62115463+neijcsilva@users.noreply.github.com>
2020-09-01 12:27:06 -0300

    HostGator

--- vendor/tecsisbr/php-classes/src/DB/Sql.php
+++ vendor/tecsisbr/php-classes/src/DB/Sql.php
@@ -4,10 +4,14 @@ namespace Tecsis\DB;
 
 class Sql {
 
-	const HOSTNAME = "127.0.0.1"; // Endereço/Nome do Servidor
-	const USERNAME = "root"; // Usuário
-	const PASSWORD = ""; // Meu banco de dados criado na conexão localhost não possui senha
-	const DBNAME = "db_ecommerce"; // Nome do Banco de Dados
+	const HOSTNAME = "192.185.212.99"; // Endereço/Nome do Servidor localhost
+	//const HOSTNAME = "127.0.0.1"; // Endereço/Nome do Servidor localhost
+	const USERNAME = "tecisn93_user"; // Usuário HOSTGATOR
+	//const USERNAME = "root"; // Usuário localhost
+	const PASSWORD = "ramien20"; // Senha do usuário HOSTGATOR
+	//const PASSWORD = ""; // Meu banco de dados criado na conexão localhost não possui senha
+	const DBNAME = "tecisn93_db"; // Nome do Banco de Dados HOSTGATOR
+	//const DBNAME = "db_ecommerce"; // Nome do Banco de Dados localhost
 
 	private $conn;
 
